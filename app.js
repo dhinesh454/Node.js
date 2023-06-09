5190550513de2f42da1d23496f5e5dfa85929834
dhinesh454 <dhinesh454@gmail.com>
2023-06-09 17:51:57 +0530

    Task 7 URL responses

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const http=require('http');
 
 
 const server = http.createServer((req,res)=>{
-  
-    res.write('<html><title>Create server</title><body><h2>Dhinesh Welcome to Node Js</h2></body></html');
-    console.log('Dhinesh');
-   
-})
+  const url =req.url;
 
-server.listen(4000)
+  if(url=='/') 
+  {
+    res.write('<html><title>Create server</title><body><h2>Dhinesh Welcome Create server Buddy  !</h2></body></html');
+  }
+
+  if(url==='/home')
+  {
+    res.write('<html><title>Create server</title><body><h2>Welcome home</h2></body></html');
+  }
+
+
+  if(url==='/about')
+  {
+    res.write('<html><title>Create server</title><body><h2> Welcome to About Us page</h2></body></html');
+  }
+
+
+  if(url==='/page')
+  {
+    res.write('<html><title>Create server</title><body><h2> Welcome to About Us page</h2></body></html');
+  }
+
+  
+
+   
+});
+
+server.listen(4000);
